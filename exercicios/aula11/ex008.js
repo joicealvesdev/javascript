c24608fd63aa8e42de6f2527b3248313ef142803
Joice Alves <joicealvesrt18@gmail.com>
2025-11-26 12:17:38 -0300

    new

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples//
+  console.log(`Voce ultrapassou o limite permitido. Voce foi multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log ('Voce é Brasileiro') }
+
+ else {
+    console.log ('Voce é Estrangeiro')
+}
+
