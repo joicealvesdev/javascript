a338f7532ef6d8f8b35a3e7c9d89948a653fbc07
Joice Alves <joicealvesrt18@gmail.com>
2025-12-03 18:02:27 -0300

    news

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+while (c < 6)
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+ */
+
+
