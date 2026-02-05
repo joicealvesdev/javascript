ff93048c4a9f26c686974c94144d77e6b889a281
Joice Alves <joicealvesrt18@gmail.com>
2026-02-05 13:32:17 -0300

    new objects

--- open-editors/objects/exercises objects/ex08.js
+++ open-editors/objects/exercises objects/ex08.js
@@ -1,3 +1,10 @@
 function tv (name, model) {
-    
+    this.name = name;
+    this.model = model;
 }
+
+const tv1 = new tv ("TV AOC" , 43 );
+const tv2 = new tv ("TV SANSUNG" , 50); 
+
+console.log(tv1.name); 
+console.log(tv2.model);
