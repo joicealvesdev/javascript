8b78a71ce303afac1951dfd2e4a909342867ebc6
Joice Alves <joicealvesrt18@gmail.com>
2026-02-05 14:29:31 -0300

    new exercises

--- open-editors/objects/exercises objects/ex07.js
+++ open-editors/objects/exercises objects/ex07.js
@@ -3,12 +3,16 @@ function pc (name , model) {
     this.model = model;
 }
 
-const pc1 = new pc ("AMD ", "Processador Intel i5 9Geração ")
+const pc1 = new pc ("AMD", "Processador Intel i5 9Geração ")
 
 const pc2 = new pc ("Duex" , "Processador Intel  i 5 9Geração");
 
 
 console.log(pc1.name);
 console.log(pc2.model); 
+
+
+
+
 
 
--- open-editors/objects/exercises objects/ex08.js
+++ open-editors/objects/exercises objects/ex08.js
@@ -8,3 +8,4 @@ const tv2 = new tv ("TV SANSUNG" , 50);
 
 console.log(tv1.name); 
 console.log(tv2.model);
+
