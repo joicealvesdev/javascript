b695425cc6f46860b376127691ce69e5096c5616
Joice Alves <joicealvesrt18@gmail.com>
2026-02-07 21:50:50 -0300

    editon

--- open-editors/callback/callback.js
+++ open-editors/callback/callback.js
@@ -37,3 +37,4 @@ console.log('Fim do programa');
 
 
 
+
--- open-editors/localstorange/exercicies/exemplo01/index.js
+++ open-editors/localstorange/exercicies/exemplo01/index.js
@@ -1,4 +1,37 @@
-localStorage.setItem("cor" , "Azul")
+/*localStorage.setItem("cor" , "Azul")
  const nome = localStorage.getItem("nome")
-console.log(nome)
+console.log(nome) */
+
+
+
+/*localStorage.setItem("user:" , "Joice")
+localStorage.setItem("número:" , "13")
+
+const user = localStorage.getItem("user")
+const número = localStorage.getItem("número")
+
+console.log(user) */
+
+
+localStorage.setItem("usuario:", "Max")
+const usuario = localStorage.getItem("usuario")
+console.log(usuario)
+
+
+localStorage.setItem("name:" , "Thais")
+const name = localStorage.getItem("name")
+console.log(name)
+
+localStorage.setItem("object:" , "table")
+const object = localStorage.getItem("object")
+console.log(object)
+
+
+localStorage.setItem("password:" , "375382jh")
+const password = localStorage.getItem("password")
+console.log(password)
+
+localStorage.setItem("enterprise:" , "Amazon")
+const enterprise = localStorage.getItem("enterprise")
+console.log(enterprise)
 
