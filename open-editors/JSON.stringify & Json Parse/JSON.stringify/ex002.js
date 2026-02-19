3ad35679665d284aed6ccb65d1a295a317a7c5e5
Joice Alves <joicealvesrt18@gmail.com>
2026-02-19 11:41:09 -0300

    manipulation json

--- open-editors/JSON.stringify & Json Parse/JSON.stringify/ex002.js
+++ open-editors/JSON.stringify & Json Parse/JSON.stringify/ex002.js
@@ -1,4 +1,4 @@
-const Person = {
+/*const Person = {
     name: "Joice",
     idade: "17 anos",
     cidade: "Belo Jardim"
@@ -8,6 +8,50 @@ console.log(`Me chamo`, Person.name + ' e tenho ' ,Person.idade, ' e atualmente 
 
 
 const texto = JSON.stringify(Person);  //utilizando JsonStringify()
-console.log(texto);                    //o Objeto virou uma Stirng Pura em JsonStringify 
+console.log(texto);       */             //o Objeto virou uma Stirng Pura em JsonStringify 
                                       //Essa String não é mais um objeto que posso acessar com Person.name, 
                                       //È agora --apenas texto--
+
+// ------------------------------------------------------------------------------------//
+
+//Inverso Json Parse 
+
+//Se O JSON.stringify() tira uma foto textual do objeto , O Json.parse() pega essa foto (string Json) e revive como objeto novamente
+
+
+
+//--------------------------------//
+
+//EXEMPLO PRATICO 
+
+
+//temos um objeto 
+
+const person = {name: "Joice", idade: "17 anos" , cidade: "Belo Jardim"};
+
+//Transformamos em string JSON
+
+const texto = JSON.stringify(person);
+console.log(texto);
+
+
+//Agora Usamos o JSON.parse para reviver 
+
+const objetoNovo = JSON.parse(texto);
+console.log(objetoNovo.name); // "Joice"
+console.log(objetoNovo.idade); //"17 anos"
+console.log(objetoNovo.cidade); // "Belo Jardim"
+
+
+ /*
+Passo 1 Objeto 
+Passo 2 Transformar o Objeto em String pura (JsonStringify)
+Passo 3 Inverter a String pura para um novo objetoNovo, resumidamente, retornando o valor anterior */
+
+
+
+
+
+
+
+
