e64b21375aaeed4b08dc26360f694ab6c46e344a
Joice Alves <joicealvesrt18@gmail.com>
2026-02-24 07:59:29 -0300

    testing

--- open-editors/Regex/validador-de-formulario/validador.js
+++ open-editors/Regex/validador-de-formulario/validador.js
@@ -15,3 +15,10 @@ function validadorEmmail(email) {
 
 console.log(validadorEmmail("jussara@gmail.com"));
 
+function validadorEmail2(email){
+    let regex = /^\w+@\w+\.\w{2,3}$/;
+    return regex.test(email);
+}
+
+console.log(validadorEmail2("joicealves@gmail.com"))
+console.log(validadorEmail2("email.com"));
