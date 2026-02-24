d1b479cbd1915d7033016ca4189470f284962f8a
Joice Alves <joicealvesrt18@gmail.com>
2026-02-24 08:26:00 -0300

    testing

--- open-editors/Regex/validador-de-formulario/validador3.js
+++ open-editors/Regex/validador-de-formulario/validador3.js
@@ -8,6 +8,29 @@ console.log(validarSenha("senha"));
 
 
 
+function validarSenha1(senha) {
+    let regex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(senha);
+}
+
+console.log(validarSenha1("Senha567"));
+
+
+function validadorSenha2(senha){
+    let regex =/^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(senha);
+}
+
+console.log(validadorSenha2("Senha239103"));
+console.log(validarSenha1("senha09"));
+
+function validarsenha3(senha){
+    let regex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(senha);
+}
+
+console.log(validarsenha3("SenhaMiojinho123"))
+
 
 
 
