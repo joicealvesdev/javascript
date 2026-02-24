b548ac34f7693ad7cf4614ea9a5c7d5a5cdb17eb
Joice Alves <joicealvesrt18@gmail.com>
2026-02-24 08:16:00 -0300

    testing

--- open-editors/Regex/validador-de-formulario/validador2.js
+++ open-editors/Regex/validador-de-formulario/validador2.js
@@ -5,3 +5,25 @@ function validadorCpf(cpf){
 
 console.log(validadorCpf("08967846508"));
 console.log(validadorCpf("12345"));
+
+function validadorCpf1(cpf){
+    let regex = /^\d{11}$/;
+    return regex.test(cpf);
+}
+
+console.log(validadorCpf1("34576589034"));
+
+function validarCpf2 (cpf){
+    let regex = /^\d{11}$/;
+    return regex.test(cpf);
+}
+
+console.log(validarCpf2("23423423456"));
+
+
+function validarCpf3(cpf) {
+    let regex = /^\d{11}$/;
+    return regex.test(cpf);
+}
+
+console.log(validarCpf3("1234"));
