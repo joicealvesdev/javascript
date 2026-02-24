c7cd74b18e6c471e0dcb86c7d5efcdbc6cbfb101
Joice Alves <joicealvesrt18@gmail.com>
2026-02-24 07:50:12 -0300

    new

--- open-editors/Regex/validador-de-formulario/validador.js
+++ open-editors/Regex/validador-de-formulario/validador.js
@@ -5,3 +5,13 @@ function validadorEmail(email){
 
 console.log(validadorEmail("joice@gmail.com"));
 console.log(validadorEmail("joice@gmail"));
+
+
+
+function validadorEmmail(email) {
+    let regex = /^\w+@\w+\.\w{2,3}$/;
+    return regex.test(email);
+}
+
+console.log(validadorEmmail("jussara@gmail.com"));
+
--- open-editors/Regex/validador-de-formulario/validador4.js
+++ open-editors/Regex/validador-de-formulario/validador4.js
@@ -7,3 +7,5 @@ console.log(validadorCep("12345-678"));
 console.log(validadorCep("12843"));
 
 
+
+
